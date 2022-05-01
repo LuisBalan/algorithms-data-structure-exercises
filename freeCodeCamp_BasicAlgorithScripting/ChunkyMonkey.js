67b9702bfea494e45f5c2fc0888f338bfe579c29
LuisBalan <jlbalcz@gmail.com>
2022-05-01 10:53:07 -0500

    add ChunkyMonkey exercise from freeCodeCamp basic algorith and scripting JS course section

---
+++ freeCodeCamp_BasicAlgorithScripting/ChunkyMonkey.js
@@ -0,0 +1,36 @@
+/*
+Chunky Monkey
+
+Write a function that splits an array (first argument)
+into groups the length of size (second argument) and returns
+them as a two-dimensional array.
+*/
+
+function chunkArrayInGroups(arr, size) {
+    let arrsContainer = [];
+  
+    while(arr.length !== 0){
+      if(arr.length >= size){
+        let sizedArr = [];
+        for(let i = 0; i < size; i++){
+          sizedArr.push(arr.shift());
+        };
+        arrsContainer.push(sizedArr);    
+      }else{
+        let sizedArr = [];
+        for(let i = 0; i < arr.length ; i++){
+          sizedArr.push(arr.shift());
+        };
+        arrsContainer.push(sizedArr);
+      };
+    };
+    return arrsContainer;
+  };
+
+ console.log(["a", "b", "c", "d"], " : ", chunkArrayInGroups(["a", "b", "c", "d"], 2)) 
+ console.log([0, 1, 2, 3, 4, 5], " : ", chunkArrayInGroups([0, 1, 2, 3, 4, 5], 3)) 
+ console.log([0, 1, 2, 3, 4, 5], " : ", chunkArrayInGroups([0, 1, 2, 3, 4, 5], 2))  
+ console.log([0, 1, 2, 3, 4, 5], " : ", chunkArrayInGroups([0, 1, 2, 3, 4, 5], 4))
+ console.log([0, 1, 2, 3, 4, 5, 6], " : ", chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6], 3)) 
+ console.log([0, 1, 2, 3, 4, 5, 6, 7, 8], " : ", chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 2))
+ console.log([0, 1, 2, 3, 4, 5], " : ",chunkArrayInGroups([0, 1, 2, 3, 4, 5], 4)) 
